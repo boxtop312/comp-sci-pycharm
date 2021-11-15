4effa485d62e5a619c4259f0daff0ffd228e099a
Alex <owsleymiranda@gmail.com>
2021-11-15 11:30:04 -0600

    commit

--- login-page-boxtop312/canvas.py
+++ login-page-boxtop312/canvas.py
@@ -8,11 +8,13 @@ root.geometry("400x500")
 
 
 #### CONTROLLER ####
-radius_changer = Scale(root)
+radius_changer = Scale(root, from_ = 10, to = 200, label = "10 <-------------RADIUS--------------> 200", orient = HORIZONTAL)
+radius_changer.place(x=50,y=425,width=300)
 
 #### VIEW ####
 display = Canvas(root, bg = "Teal")
 display.place(x = 0, y = 0, width = 400, height = 400)
 
+circle = display.create_oval()
 
 root.mainloop()
