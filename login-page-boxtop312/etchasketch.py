27bf43b065bcd1a59a42e1d7085a71740a378919
Alex Miranda <owsleymiranda@gmail.com>
2021-11-17 12:33:49 -0600

    commit

--- login-page-boxtop312/etchasketch.py
+++ login-page-boxtop312/etchasketch.py
@@ -38,10 +38,10 @@ def clearscreen():
 
 # CONTROLLER #
 up_down = Scale(window, from_ = 0, to = 600,font = ("Lobster 1.4",10), orient = VERTICAL, variable = newY, command = draw_line)
-up_down.place(x=10,y=599,height = 200)
+up_down.place(x=10,y=598,height = 200)
 
 up_down_label = Label(window, text = "u\np\n \n&\n \nd\no\nw\nn\n", font = ("Lobster 1.4",10))
-up_down_label.place(x = 52, y = 600)
+up_down_label.place(x = 53, y = 600)
 
 left_right = Scale(window, from_ = 0, to = 600,font = ("Lobster 1.4",10),label = "left & right", orient = HORIZONTAL, variable = newX, command = draw_line)
 left_right.place(x = 200, y = 600, width = 200)
@@ -49,7 +49,7 @@ left_right.place(x = 200, y = 600, width = 200)
 blackbutt = Button(window, fg = "white", bg = "black",font = ("Lobster1.4",15), text = "Black", command = lambda :colorchanger("Black"))
 blackbutt.place(x = 200, y = 700, width = 75, height = 20)
 
-whitebutt = Button(window, fg = "black", bg = "white", font = ("Lobster 1.4",15),text = "Erase", command = lambda :colorchanger("White"))
+whitebutt = Button(window, fg = "black", bg = "white", font = ("Lobster 1.4",15),text = "White", command = lambda :colorchanger("White"))
 whitebutt.place(x = 200,y = 720,width = 75,height = 20)
 
 redbutt = Button(window, fg = "black", bg = "Red", font = ("Lobster 1.4",15), text = "Red", command = lambda :colorchanger("Red"))
@@ -74,7 +74,7 @@ clearbutt = Button(window,fg = "black", font = ("Lobster 1.4",15),text = "Clear"
 clearbutt.place(x = 275,y = 740,width = 75,height =20)
 
 # VIEW #
-display = Canvas(window, bg = "white")
+display = Canvas(window, bg = "teal")
 display.place(x = 0, y = 0, width = 600, height = 600)
 
 mainloop()
