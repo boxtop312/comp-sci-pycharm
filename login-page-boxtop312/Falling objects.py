ee2945c11b52516797bdd08b47152b215bc069a4
Alex Miranda <owsleymiranda@gmail.com>
2021-12-01 12:52:26 -0600

    commit

--- login-page-boxtop312/Falling objects.py
+++ login-page-boxtop312/Falling objects.py
@@ -86,19 +86,26 @@ class Ball:
 
 class Box:
     def __init__(self):
-        self.xlen = random.randint(1,10)
-        self.ylen = random.randint(1,10)
+        self.xlen = random.randint(5,25)
+        self.ylen = random.randint(5,25)
         self.color = random_color()
         self.x = random.randint(10,490)
         self.y = random.randint(10,100)
+        self.speed = 5
+        self.direction = 3.14*2
+        self.deltaX = self.speed * math.cos(self.direction)
+        self.deltaY = self.speed * math.sin(self.direction)
         self.bo = display.create_rectangle(self.x - (self.xlen/2),self.y - (self.ylen/2),self.x + (self.xlen/2),self.y + (self.ylen/2),fill = self.color)
 
     def fall(self):
+        left, top, right, bottom = self.coords()
         display.move(self.bo,0,random.randint(1,5))
         self.x = random.randint(10, 490)
         self.y = random.randint(10, 100)
-        if self.y >= 700:
-            display.coords(self.x - (self.xlen/2),self.y - (self.ylen/2),self.x + (self.xlen/2),self.y + (self.ylen/2))
+        if bottom >= 700:
+            display.coords(self.bo,self.x - (self.xlen/2),self.y - (self.ylen/2),self.x + (self.xlen/2),self.y + (self.ylen/2))
+        display.after(10)
+        display.update()
 
     def coords(self):  # returns the coordinates for the top left corner of the object and the coordinates for the bottom right corner
         return display.coords(self.bo)
@@ -112,15 +119,22 @@ display = Canvas(root, bg=random_color())
 display.place(x=0, y=0, width=500, height=700)
 
 ##### CODE FOR THE GAME ######
-first = Box()
+# first = Ball()
+secont = Box()
 
 balls = []
 boxes = []
-for n in range(15):
+for n in range(20):
     boxes.append(Box())
-    balls.append(Ball())
+    # balls.append(Ball())
 
 while True:
-    first.fall()
+    # first.move(balls)
+    secont.fall()
+    # for ball in balls:
+    #    ball.move(balls)
     for box in boxes:
-       box.fall()
+        box.fall()
+
+
+root.mainloop()
