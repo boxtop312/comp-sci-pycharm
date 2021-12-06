d406f778ca131e2f145952d0ff11989f4023cea0
Alex Miranda <owsleymiranda@gmail.com>
2021-12-06 12:02:47 -0600

    commit

--- login-page-boxtop312/Falling objects.py
+++ login-page-boxtop312/Falling objects.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random, math
+from PIL import Image, ImageTk
 
 root = Tk()
 root.title("Falling Objects")
@@ -7,7 +8,9 @@ root.geometry("500x700")
 
 
 #### DATA ####
-# img = img.open()
+img = Image.open("whale.png")
+img = img.resize((50,50),Image.ANTIALIAS)
+whale = ImageTk.PhotoImage(img)
 
 def random_color():
     hex = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
@@ -111,6 +114,72 @@ class Box:
         return display.coords(self.bo)
 
 
+class Whale:
+    def __init__(self, img):
+        self.img = img
+        self.direction = 3.14 / 2
+        self.speed = 10
+        self.deltaX = self.speed * math.cos(self.direction)
+        self.deltaY = self.speed * math.sin(self.direction)
+        self.oct = display.create_image(200, 200, image=self.img)
+        root.bind("<Left>", self.left)
+        root.bind("<Right>", self.right)
+
+    def move(self,whales):
+        left, top = display.coords(self.oct)
+        right = left + 50
+        bottom = top + 50
+        if left < 0 or right > 500:
+            self.deltaX = self.deltaX * -1
+            display.move(self.oct, self.deltaX, self.deltaY)
+        if top < 10 or bottom > 695:
+            self.deltaY = self.deltaY * -1
+            display.move(self.oct, self.deltaX, self.deltaY)
+
+        display.move(self.oct, self.deltaX, self.deltaY)
+        # for oct in whales:
+        #     self.collision(oct)
+        # display.after(10)
+        # display.update()
+
+    def left(self, event):
+        self.direction = self.direction + .2
+        self.deltaX = self.speed * math.cos(self.direction)
+        self.deltaY = -1 * self.speed * math.sin(self.direction)
+
+    def right(self, event):
+        self.direction = self.direction - .2
+        self.deltaX = self.speed * math.cos(self.direction)
+        self.deltaY = -1 * self.speed * math.sin(self.direction)
+
+
+    def collision(self, object):
+        bLeft, bTop, bRight, bBottom = self.coords()
+        oLeft, oTop, oRight, oBottom = object.coords()
+        # Left - when ball hits the left side of the object
+        if bLeft < oRight and bRight > oRight and bTop < oBottom and bBottom > oTop:
+            self.deltaX = self.deltaX * -1
+            display.move(self.oct, self.deltaX, self.deltaY)
+
+        # Right - when ball hits the right side of the object
+        if bRight > oLeft and bLeft < oLeft and bTop < oBottom and bBottom > oTop:
+            self.deltaX = self.deltaX * -1
+            display.move(self.oct, self.deltaX, self.deltaY)
+
+        # Top - when ball hits the top of the object
+        if bBottom > bTop and bTop < oTop and bLeft < oRight and bRight > oLeft:
+            self.deltaY = self.deltaY * -1
+            display.move(self.oct, self.deltaX, self.deltaY)
+
+        # Bottom - when ball hits the bottom of the object
+        if bTop < oBottom and bBottom > oBottom and bRight > oLeft and bLeft < oRight:
+            self.deltaY = self.deltaY * -1
+            display.move(self.oct, self.deltaX, self.deltaY )
+
+    def coords(self):
+        # returns the coordinates for the top left corner of the object and the coordinates for the bottom right corner
+        return display.coords(self.oct)
+
 #### CONTROLLERS ####
 
 
@@ -119,22 +188,25 @@ display = Canvas(root, bg=random_color())
 display.place(x=0, y=0, width=500, height=700)
 
 ##### CODE FOR THE GAME ######
-# first = Ball()
 secont = Box()
 
 balls = []
 boxes = []
+whales = []
+whales.append(Whale(whale))
+balls.append(Ball())
 for n in range(20):
     boxes.append(Box())
-    # balls.append(Ball())
 
 while True:
-    # first.move(balls)
     secont.fall()
-    # for ball in balls:
-    #    ball.move(balls)
+    for wha in whales:
+        wha.move(boxes)
+    for ball in balls:
+       ball.move(balls)
     for box in boxes:
         box.fall()
+
 
 
 root.mainloop()
