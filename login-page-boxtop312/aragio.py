67ae011e47129bb8d3dab525652df870a6c4f752
Alex Miranda <owsleymiranda@gmail.com>
2022-01-22 11:52:18 -0600

    commit

--- login-page-boxtop312/aragio.py
+++ login-page-boxtop312/aragio.py
@@ -1,4 +1,6 @@
-import pygame, random, math
+import pygame
+import random
+import math
 
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
@@ -18,6 +20,16 @@ def random_color():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
+def distance(obj1, obj2):
+    x1 = obj1.rect.left
+    y1 = obj1.rect.top
+    x2 = obj2.rect.left
+    y2 = obj2.rect.top
+    x = x1-x2
+    y = y1-y2
+    return math.sqrt((x**2)+(y**2))
+
+
 class Food(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super(Food, self).__init__()
@@ -26,7 +38,7 @@ class Food(pygame.sprite.Sprite):
         self.surf.set_colorkey("black")
         self.radius = 6
         # Arguments for draw.circle(surface,(R,G,B),(x,y for location on surface),radius, width edge )
-        pygame.draw.circle(self.surf, (random_color()), (self.radius, self.radius), self.radius, 3)
+        pygame.draw.circle(self.surf, (random_color()), (self.radius, self.radius), self.radius, 0)
         self.rect = self.surf.get_rect()
         self.rect.left = x
         self.rect.top = y
@@ -37,9 +49,19 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.radius = 25
+        self.color = random_color()
         self.surf = pygame.Surface((self.radius * 2, self.radius * 2))
-        pygame.draw.circle(self.surf, random_color(), (self.radius, self.radius), self.radius, 10)
+        self.surf.fill((0, 0, 0))
+        self.surf.set_colorkey((0, 0, 0))
+        pygame.draw.circle(self.surf, self.color, (self.radius, self.radius), self.radius, 0)
         self.rect = self.surf.get_rect()
+        self.rect.left = 225
+        self.rect.top = 255
+        self.x = 250
+        self.y = 250
+
+    def collide(self, obj):
+        return distance(self, obj) <= self.radius/7 + obj.radius
 
 
 # Initialize pygame
@@ -52,7 +74,7 @@ SCREEN_HEIGHT = 500
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+player = Player()
 # Group contains all of the food objects
 comida = pygame.sprite.Group()
 for f in range(20):
@@ -62,6 +84,7 @@ for f in range(20):
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(comida)
+all_sprites.add(player)
 
 # Variable to keep our main loop running
 running = True
@@ -85,6 +108,31 @@ while running:
     for y in range(0, SCREEN_HEIGHT, 25):
         pygame.draw.line(screen, (255, 255, 255), (0, y), (SCREEN_WIDTH, y))
 
+    mx, my = pygame.mouse.get_pos()
+    # player.rect.left = mx - player.radius
+    # player.rect.top = my - player.radius
+    x1 = mx - player.rect.left - player.radius
+    y1 = my - player.rect.top - player.radius
+    hyp = math.sqrt((x1 ** 2) + (y1 ** 2))
+    if hyp == 0:
+        hyp = 0.0000001
+    player.deltaX = ((0.1 * x1) / hyp)
+    player.deltaY = ((0.1 * y1) / hyp)
+
+    player.x += player.deltaX
+    player.y += player.deltaY
+    player.rect.left = player.x
+    player.rect.top = player.y
+
+    for i in all_sprites:
+        if player != i and player.collide(i):
+            i.kill()
+            player.radius += i.radius
+            player.surf = pygame.Surface((player.radius * 2, player.radius * 2))
+            player.surf.fill((0, 0, 0))
+            player.surf.set_colorkey((0, 0, 0))
+            pygame.draw.circle(player.surf, player.color, (player.radius, player.radius), player.radius, 0)
+            player.rect = player.surf.get_rect()
     for a in all_sprites:
         screen.blit(a.surf, a.rect)
 
---
+++ login-page-boxtop312/wordle solver.py
@@ -0,0 +1,3 @@
+allwords = open("words.txt","r")
+wordbank = allwords.read()
+wordbank = wordbank.split()  # now wordbank is a list
