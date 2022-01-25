49f4831db0c984f464d1a5d2fa25127feda17288
Alex Miranda <owsleymiranda@gmail.com>
2022-01-25 12:25:30 -0600

    commit

--- login-page-boxtop312/wordle solver.py
+++ login-page-boxtop312/wordle solver.py
@@ -1,20 +1,34 @@
 from heapq import merge
-wordbank = open("words.txt", "r")
-wordbank = wordbank.read()
-wordbank = wordbank.split()  # now word bank is a list
+# wordbank = open("words.txt", "r")
+# wordbank = wordbank.read()
+# wordbank = wordbank.split()  # now word bank is a list
+#
+# fiveletterwords = open("5leterwords.txt", "r")
+# fivewordbank = fiveletterwords.read()
+# fivewordbank = fivewordbank.split()  # now five word bank is also a list
+#
+# wordbank = list(merge(wordbank, fivewordbank))  # now word bank is a combination of word bank and five word bank as one
+#
+# oldwords = []
+# for i in wordbank:
+#     oldwords.append(i)
 
-fiveletterwords = open("5leterwords.txt", "r")
-fivewordbank = fiveletterwords.read()
-fivewordbank = fivewordbank.split()  # now five word bank is also a list
 
-wordbank = list(merge(wordbank, fivewordbank))  # now word bank is a combination of word bank and five word bank as one
+def wordlesolver():
+    wordbank = open("words.txt", "r")
+    wordbank = wordbank.read()
+    wordbank = wordbank.split()  # now word bank is a list
 
-oldwords = []
-for i in wordbank:
-    oldwords.append(i)
+    fiveletterwords = open("5leterwords.txt", "r")
+    fivewordbank = fiveletterwords.read()
+    fivewordbank = fivewordbank.split()  # now five word bank is also a list
 
+    wordbank = list(
+        merge(wordbank, fivewordbank))  # now word bank is a combination of word bank and five word bank as one
 
-def wordlesolver(wordbank, oldwords):
+    oldwords = []
+    for i in wordbank:
+        oldwords.append(i)
     removedwords = []
     wordlen = 5
     notletters = (input("input the letters that are not in the word:"))
@@ -76,13 +90,48 @@ def wordlesolver(wordbank, oldwords):
     print(wordbank)
 
 
-def generalpurpose(wordbank, oldwords):
+def generalpurpose():
     removedwords = []
     wordlen = int(input("input the number of letters in the word"))
     notletters = (input("input the letters that are not in the word:"))
     fixedletters = str(input("input the letters that are correct, use underscores for the blank spaces \n for example "
                              "o_u_t:"))
+    if wordlen == 5:
+        wordbank = open("words.txt", "r")
+        wordbank = wordbank.read()
+        wordbank = wordbank.split()  # now word bank is a list
 
+        fiveletterwords = open("5leterwords.txt", "r")
+        fivewordbank = fiveletterwords.read()
+        fivewordbank = fivewordbank.split()  # now five word bank is also a list
+
+        wordbank = list(
+            merge(wordbank, fivewordbank))  # now word bank is a combination of word bank and five word bank as one
+
+        oldwords = []
+        for i in wordbank:
+            oldwords.append(i)
+    else:
+        wordbank = open("words.txt", "r")
+        wordbank = wordbank.read()
+        wordbank = wordbank.split()  # now word bank is a list
+
+        fiveletterwords = open("5leterwords.txt", "r")
+        fivewordbank = fiveletterwords.read()
+        fivewordbank = fivewordbank.split()  # now five word bank is also a list
+
+        wordbank = list(
+            merge(wordbank, fivewordbank))  # now word bank is a combination of word bank and five word bank as one
+
+        twoletterwords = open("2leterwords.txt", "r")
+        twowordbank = twoletterwords.read()
+        twowordbank = twowordbank.split()
+
+        wordbank = list(merge(wordbank, twowordbank))
+
+        oldwords = []
+        for i in wordbank:
+            oldwords.append(i)
 
     for i in oldwords:
         if len(i) != wordlen and i not in removedwords:
@@ -98,14 +147,6 @@ def generalpurpose(wordbank, oldwords):
                     # print("removed word with letters in not letters")
                     print(len(wordbank))
                     break
-            for q in unfixedletters:
-                if q != "_":
-                    if q not in i.lower() and i not in removedwords:
-                        removedwords.append(i)
-                        wordbank.remove(i)
-                        # print("removed word with letters not in yellow letters")
-                        print(len(wordbank))
-                        break
             for ii in range(0, len(i)):
                 if i not in removedwords and fixedletters[ii] != "_" and fixedletters[ii] != i[ii].lower():
                     removedwords.append(i)
@@ -131,6 +172,6 @@ def generalpurpose(wordbank, oldwords):
 
 wordgame = int(input("input what word game you are solving\n 0 for wordle 1 for general purpose: "))
 if wordgame == 0:
-    wordlesolver(wordbank, oldwords)
+    wordlesolver()
 elif wordgame == 1:
-    generalpurpose(wordbank, oldwords)
+    generalpurpose()
