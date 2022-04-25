e0864c32a2ca5eea9885e22922482a55ddf02f3d
Alex Miranda <owsleymiranda@gmail.com>
2022-04-25 13:33:44 -0500

    commit

--- login-page-boxtop312/public and private key encryption.py
+++ login-page-boxtop312/public and private key encryption.py
@@ -1,27 +1,56 @@
+import random
 
-def calculate_public_key(p, q):
-    return p * q
-
-
-def encrypt(n, e, letter):
-    return (ord(letter) ** e) % n
-
-
-def encrypt_message(n, e, message):
-    newlist = []
-    for i in message:
-        newlist.append(encrypt(n, e, i))
-    return newlist
+e = 64049653
+p = 180406337
+q = 385713907
+N = q * p
+T = (p - 1) * (q - 1)
 
 
-def calculate_private_key(p, q):
-    return (p - 1) * (q - 1)
+def isprime(x):
+    if x > 1:
+        for n in range(2, x):
+            if (x % n) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
 
 
+def gcd(p, q):
+    # Create the gcd of two positive integers.
+    while q != 0:
+        p, q = q, p % q
+    return p
 
-def decrypt(p, q, r):
-    return (r ** calculate_private_key(p, q)) % calculate_public_key(p, q)
+
+def is_coprime(x, y):
+    return gcd(x, y) == 1
 
 
-print(encrypt(calculate_public_key(3, 5), 69, "a"))
-print(chr((decrypt(3, 5, 6))))
+def primes_less_than(n):
+    all_primes = []
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while p * p <= n:
+        if prime[p]:
+            for i in range(p ** 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime[0] = False
+    prime[1] = False
+    # Print all prime numbers
+    for p in range(n + 1):
+        if prime[p]:
+            all_primes.append(p)
+    return all_primes
+
+
+for d in primes_less_than(999999999):
+    if (e*d) % T == 1:
+        print(d)
+        break
+    else:
+        print(d, 0)
+
