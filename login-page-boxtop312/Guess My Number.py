c1b45ddb97b95882da1b515317fe35cf6566a5bc
Alex Miranda <owsleymiranda@gmail.com>
2023-06-23 16:37:42 -0400

    Commmit

--- login-page-boxtop312/Guess My Number.py
+++ login-page-boxtop312/Guess My Number.py
@@ -1,1 +1,14 @@
-print("hello world")
+import random as r
+
+rand = r.randint(1, 100)
+thing = int(input("Guess my number: "))
+guessCounter = 0
+while thing != rand:
+    if thing < rand:
+        guessCounter += 1
+        print("Too low")
+    elif thing > rand:
+        guessCounter += 1
+        print("Too high")
+    thing = int(input("Guess my number: "))
+print("You got it!!\nin " + str(guessCounter) + " guesses")
